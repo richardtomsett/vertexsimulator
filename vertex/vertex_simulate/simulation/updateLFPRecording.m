94fd9b3589e11a700f33e0eb40438289e5c2ca91
Richard Tomsett <r@autap.se>
2014-11-06 14:47:40 +0900

    Fixed error in LFP calculation: missing minus sign! This means that any LFP calculations with prior VERTEX versions will have the correct magnitude but wrong sign.

--- vertex/vertex_simulate/simulation/updateLFPRecording.m
+++ vertex/vertex_simulate/simulation/updateLFPRecording.m
@@ -2,11 +2,11 @@ function [RecVar] = ...
   updateLFPRecording(RS,NeuronModel,RecVar,lineSourceModCell,iGroup,recTimeCounter)
 
 if isfield(RS, 'LFPoffline') && RS.LFPoffline
-  RecVar.LFPRecording{iGroup}(:,:,p_recTimeCounter) = NeuronModel{iGroup}.I_ax;
+  RecVar.LFPRecording{iGroup}(:,:,p_recTimeCounter) = -NeuronModel{iGroup}.I_ax;
 else
   for iElectrode = 1:RS.numElectrodes
     RecVar.LFPRecording{iGroup}(iElectrode,recTimeCounter) = ...
-      sum(sum( (NeuronModel{iGroup}.I_ax) .* ...
+      sum(sum( (-NeuronModel{iGroup}.I_ax) .* ...
       lineSourceModCell{iGroup, iElectrode} ));
   end
 end
